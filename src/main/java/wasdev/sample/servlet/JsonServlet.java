2e0337d32cd12f22fb0ece1cff249a2e6e98f2c1
IBM_ADMIN <prakashanand@in.ibm.com>
2016-08-16 17:24:16 +0100

    weather path

--- src/main/java/wasdev/sample/servlet/JsonServlet.java
+++ src/main/java/wasdev/sample/servlet/JsonServlet.java
@@ -41,7 +41,7 @@ public class JsonServlet extends HttpServlet {
 	    BufferedReader reader = null;
 		
 		String weather="";
-		String urlString="https://fcb61d2d-3a6d-4c39-9b75-70172d3df53d:vlX9X7iVGd@twcservice.stage1.mybluemix.net/api/weather/v1/location/30339%3A4%3AUS/almanac/daily.json?start=0112";
+		String urlString="https://2c435666-86e9-4041-8c17-46c807cf59fc:7lnNFiTeLZ@twcservice.stage1.mybluemix.net/api/weather/v1/location/30339%3A4%3AUS/almanac/daily.json?start=0112";
 		try {
 			
 	        URL url = new URL(urlString);
