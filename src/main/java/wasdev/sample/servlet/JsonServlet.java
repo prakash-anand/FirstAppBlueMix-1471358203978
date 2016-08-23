673b32eb89061e15f221874e5d3e2d52418bee66
IBM_ADMIN <prakashanand@in.ibm.com>
2016-08-23 14:43:04 +0100

    JsonServlet

--- src/main/java/wasdev/sample/servlet/JsonServlet.java
+++ src/main/java/wasdev/sample/servlet/JsonServlet.java
@@ -1,44 +1,29 @@
 package wasdev.sample.servlet;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-//import java.io.PrintWriter;
-import java.net.HttpURLConnection;
+
 import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
-import com.google.gson.stream.JsonReader;
 import com.ibm.watson.developer_cloud.personality_insights.v2.PersonalityInsights;
-import com.ibm.watson.developer_cloud.personality_insights.v2.model.Content;
+
 import com.ibm.watson.developer_cloud.personality_insights.v2.model.Profile;
 import com.ibm.watson.developer_cloud.personality_insights.v2.model.ProfileOptions;
 import com.ibm.watson.developer_cloud.personality_insights.v2.model.ProfileOptions.Language;
-import com.ibm.watson.developer_cloud.util.GsonSingleton;
+
 
 
 
